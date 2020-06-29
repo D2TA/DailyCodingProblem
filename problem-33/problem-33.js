a2fe95c67a41f11ea60ca552c18f1f0dc1788a09
moochii <david.phong.ta@gmail.com>
2020-06-28 23:55:06 -0400

    problem-33

--- problem-33/problem-33.js
+++ problem-33/problem-33.js
@@ -1,1 +1,24 @@
 var inputArray = [2, 1, 5, 7, 2, 0, 5];
+
+function RollingMedian(inputArray){
+    for (i = 0; i <= inputArray.length -1; i++){
+        ListArray = [];
+        for (j = 0; j <= i; j++){
+            ListArray.push(inputArray[j]);
+        };
+        ListArray.sort( function (a,b) {return a - b});
+        console.log(ListArray);
+    
+        if (ListArray.length%2 == 0){
+            MiddleIndex1 = Math.floor( (ListArray.length - 1)/2);
+            MiddleIndex2 = MiddleIndex1 + 1;
+            AverageValue = ((ListArray[MiddleIndex1]) + (ListArray[MiddleIndex2]))/2
+            console.log('The Rolling Median of the Sequence is: ' + AverageValue);
+        } else {
+            MiddleIndex = Math.floor(ListArray.length/2);
+            console.log('The Rolling Median of the Sequence is: ' + ListArray[MiddleIndex]);
+        };
+    };
+};
+
+RollingMedian(inputArray);
